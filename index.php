9b1c3f78936661d347b22a5f96f8cf4e39f9eda6
joaverot <joachim.verot@etu.univ-lyon1.fr>
2016-03-08 11:48:09 +0100

    Oui1
    ja

---
+++ index.php
@@ -0,0 +1,49 @@
+ <!doctype html>
+ <html lang="fr">
+ <head>
+ 	<meta charset="utf-8" />
+ 	<meta name="keywords" content="xhtml, html5, form" />
+ 	<meta name="author" content="VicTeam" />
+ 		<title>Oui</title>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+
+<span>
+    <img src="images/...jpg" alt="Baniere" />
+</span>
+<span>
+ <img src="images/...jpg" alt="Background" />
+</span>
+
+
+
+
+
+
+<div id=connexion>
+	ul>
+		<li><a href="inscription.html">Inscription</a></li>
+		<li><a href="connexion.html">Connexion</a></li>
+</div>
+<nav >
+	<ul>
+		<li><a href="home.html">Home</a></li>
+		<li><a href="recherche.html">Recherche</a></li>
+		<li><a href="recherche_mot.html">Recherche mots clés</a></li>
+		
+	
+	</ul>
+</nav>
+
+
+
+ 	<body>
+
+
+ 		<footer>	
+ 		</footer>
+
+ 	</body>
+ 	</html>
