bd3f91da2678e6317875a228061718083fe22b4b
Benoit MENARD <benoit_menard@me.com>
2016-03-10 15:48:10 +0100

    index.tpl, index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 	require("libs/Smarty.class.php"); // On inclut la classe Smarty
 	$smarty = new Smarty();
 	$smarty->assign('ESSAI','ESSAU DE VARIABLE');
-	$smarty->display('index.tpl');
+	$smarty->display('templates/index.tpl');
 ?>
