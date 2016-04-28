2904a4d24feb59ea1c1c3b6332302d5bd189a09a
Papalloon <Pierre.lannel@gmail.com>
2016-04-28 23:39:28 +0200

    Added files via upload

---
+++ WS.php
@@ -0,0 +1,26 @@
+<?php
+require_once("lib/bd/bddClass.php");
+require_once("lib/class/errorClass.php");
+Class Pathologies {
+
+	$id = $_GET['param'];
+
+	if ($id = '' ){
+		private $pathologies = "	SELECT *
+					FROM patho";
+	} else {
+		private $pathologies = "	SELECT *
+					FROM patho
+					WHERE idP = ?";
+	}
+
+	$rows = executeQuerry($pathologies,$id);
+
+$retour="<?xmlversion=\"1.0\"encoding=\"UTF-8\"?>\n<!DOCTYPElisteSYSTEM\"Pathologie.dtd\">\n<lste>\n";
+    	foreach($rows as $element)
+    {
+        $retour.="\t<pathologie>\n\t\t<nom>".$element['Description Pathologie']."</nom>\n\t</pathologie>\n";
+    }
+$retour.="</liste>";
+}
+?>
